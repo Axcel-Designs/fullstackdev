3d9f24d491ec4c71f1b3f59d0d94d600f075581a
excel <axceldesigns@gmail.com>
2025-12-11 14:40:35 +0100

    week2

--- project02/app.js
+++ project02/app.js
@@ -1,7 +1,8 @@
 const section = document.querySelectorAll("section");
 
 section.forEach((sec) => {
-  sec.addEventListener("click", (event) => {
-    alert(`Button clicked!`);
+  sec.addEventListener("click", () => {
+    const productName = sec.querySelector("h2").textContent;
+    alert(productName);
   });
 });
